6a572c93c0b9f69e82d3dfde5a09d38de5c9b59e
SanjanaSK25 <sanjana.s2523@gmail.com>
2024-08-03 11:59:36 +0530

    first commit

---
+++ models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: { 
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: String,
+        url: {
+            type: String,
+            default: 
+            "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D",
+            set: (v) => v === " " 
+            ? "https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGxhbmRzY2FwZXxlbnwwfHwwfHx8MA%3D%3D"
+            : v,
+            }   
+    },
+    price: Number,
+    location: String, 
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
